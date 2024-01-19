6ec1d1c8e9afdd925f321b9666ad8b19611b142e
LegendaryFire <tristan.balon@outlook.com>
2024-01-19 15:14:54 -0800

    Implemented basic setuptools config.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='django-responsive-tables2',
+    version='0.0.1',
+    python_requires='>=3.8',
+    install_requires=[
+        'django >= 5.0',
+    ],
+)
