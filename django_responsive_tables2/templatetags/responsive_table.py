ecfb0c8dc9518dbf378ed2016b082ae44f8ded75
LegendaryFire <tristan.balon@outlook.com>
2024-01-19 22:43:34 -0800

    Added auto search capability to the basic table template.

--- django_responsive_tables2/templatetags/responsive_table.py
+++ django_responsive_tables2/templatetags/responsive_table.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def responsive_table(context, table_id, url, table_template='django_responsive_tables2/basic_table.html', *args, **kwargs):
+def responsive_table(context, table_id, url, refresh_interval=500, table_template='django_responsive_tables2/basic_table.html', *args, **kwargs):
     table_template = loader.get_template(table_template)
-    context.push(table_url=reverse(url, args=args, kwargs=kwargs), table_id=table_id)
+    context.push(table_url=reverse(url, args=args, kwargs=kwargs), table_id=table_id, refresh_interval=refresh_interval)
     return table_template.render(context)
