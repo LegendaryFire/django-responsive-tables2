29a31969a177aa8ad34a086527bf5d67e47b1db2
LegendaryFire <tristan.balon@outlook.com>
2024-01-21 16:47:23 -0800

    Fixed bug when passing context to table template.

--- django_responsive_tables2/templatetags/responsive_table.py
+++ django_responsive_tables2/templatetags/responsive_table.py
@@ -9,4 +9,4 @@ register = template.Library()
 def responsive_table(context, table_id, url, refresh_interval=500, table_template='django_responsive_tables2/basic_table.html', *args, **kwargs):
     table_template = loader.get_template(table_template)
     context.push(table_url=reverse(url, args=args, kwargs=kwargs), table_id=table_id, refresh_interval=refresh_interval)
-    return table_template.render(context)
+    return table_template.render(context.flatten())
